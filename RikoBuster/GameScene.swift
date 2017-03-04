605867648776c0486c40d6ba1d78378e359ca67d
giginet <giginet.net@gmail.com>
2017-03-04 16:00:44 +0900

    Kill riko :hocho:

--- RikoBuster/GameScene.swift
+++ RikoBuster/GameScene.swift
@@ -23,7 +23,19 @@ class GameScene: SKScene {
         let positionX = arc4random() % 1000
         let riko = SKSpriteNode(imageNamed: "riko1")
         riko.position = CGPoint(x: Int(positionX), y: -20)
-        riko.run(SKAction.move(by: CGVector(dx: 0, dy: 30), duration: 0.5))
+        riko.run(SKAction.sequence([
+            SKAction.move(by: CGVector(dx: 0, dy: 30), duration: 0.5),
+            SKAction.wait(forDuration: 1.0),
+            SKAction.move(by: CGVector(dx: 0, dy: -30), duration: 0.5),
+            SKAction.customAction(withDuration: 0, actionBlock: { (bird, _) in
+                if let bird = bird as? SKSpriteNode,
+                    let index = self.birds.index(of: bird) {
+                    self.birds.remove(at: index)
+                }
+                
+            }),
+            SKAction.removeFromParent(),
+        ]))
         
         addChild(riko)
         birds.append(riko)
@@ -42,8 +54,21 @@ class GameScene: SKScene {
     
     func kill(bird: SKSpriteNode) {
         if let index = birds.index(of: bird) {
+            let explosion = SKSpriteNode(imageNamed: "explosion")
+            explosion.position = bird.position
+            addChild(explosion)
+            explosion.setScale(0)
+            
+            explosion.run(SKAction.sequence([
+                SKAction.scale(to: 1.0, duration: 0.25),
+                SKAction.wait(forDuration: 0.25),
+                SKAction.fadeOut(withDuration: 0.5),
+                SKAction.removeFromParent(),
+            ]))
+            
             birds.remove(at: index)
             bird.removeFromParent()
+            
         }
     }
 }
